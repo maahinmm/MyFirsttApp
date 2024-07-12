c5ad5592e835b8fe8f0717538d56c02bf9299022
maahinmm <maahinmirza@gmail.com>
2024-07-12 13:03:05 -0400

    Initial Commit

---
+++ MyFirsttApp/MyFirsttAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirsttAppApp.swift
+//  MyFirsttApp
+//
+//  Created by Maahin Mirza on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirsttAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
